419600e2d4cdf578e446b70d2f897317ce03def5
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-09-13 20:57:27 +0200

    Use the new settings API

--- settings.lua
+++ settings.lua
@@ -5,13 +5,13 @@ local function define_str(flag, default, write_to_config)
 	if value then -- This flag exists in vmg.conf, return its value
 		return value, true
 	else
-		local on_config = minetest.setting_get("vmg_" .. flag) -- get this flag in minetest.conf
+		local on_config = minetest.settings:get("vmg_" .. flag) -- get this flag in minetest.conf
 		if on_config then -- This flag exists in minetest.conf, so return its value
 			vmg.settings:set(flag, on_config)
 			return on_config, false
 		else -- Flag don't exist anywhere, so the default value will be written in settings and returned
 			if write_to_config then
-				minetest.setting_set("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
+				minetest.settings:set("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
 			end
 			vmg.settings:set(flag, default) -- write to vmg.conf
 			return default, false -- return default value
@@ -24,96 +24,71 @@ local function define_num(flag, default, write_to_config)
 	if value then -- This flag exists in vmg.conf, return its value
 		return tonumber(value), true
 	else
-		local on_config = minetest.setting_get("vmg_" .. flag) -- get this flag in minetest.conf
+		local on_config = minetest.settings:get("vmg_" .. flag) -- get this flag in minetest.conf
 		if on_config then -- This flag exists in minetest.conf, so return its value
 			vmg.settings:set(flag, on_config)
 			return tonumber(on_config), false
 		else -- Flag don't exist anywhere, so the default value will be written in settings and returned
 			if write_to_config then
-				minetest.setting_set("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
+				minetest.settings:set("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
 			end
 			vmg.settings:set(flag, default) -- write to vmg.conf
-			return default, false -- return default value
+			return tonumber(default), false -- return default value
 		end
 	end
 end
 
 local function define_bool(flag, default, write_to_config)
 	local value = vmg.settings:get_bool(flag)
-	if value ~= nil then -- This flag exists in vmg.conf, return its value
+	if value then -- This flag exists in vmg.conf, return its value
 		return value, true
 	else
-		local on_config = minetest.setting_getbool("vmg_" .. flag) -- get this flag in minetest.conf
-		if on_config ~= nil then -- This flag exists in minetest.conf, so return its value
-			vmg.settings:set(flag, tostring(on_config))
+		local on_config = minetest.settings:get_bool("vmg_" .. flag) -- get this flag in minetest.conf
+		if on_config then -- This flag exists in minetest.conf, so return its value
+			vmg.settings:set_bool(flag, on_config)
 			return on_config, false
 		else -- Flag don't exist anywhere, so the default value will be written in settings and returned
 			if write_to_config then
-				minetest.setting_setbool("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
+				minetest.settings:set_bool("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
 			end
-			vmg.settings:set(flag, tostring(default)) -- write to vmg.conf
+			vmg.settings:set_bool(flag, default) -- write to vmg.conf
 			return default, false -- return default value
 		end
 	end
 end
 
 local function define_noise(flag, default, write_to_config)
-	local value = vmg.settings:get(flag)
+	local value = vmg.settings:get_np_group(flag)
 	if value then -- This flag exists in vmg.conf, return its value
-		return vmg.string_to_noise(value), true
+		return value, true
 	else
-		local on_config = minetest.setting_get("vmg_" .. flag) -- get this flag in minetest.conf
+		local on_config = minetest.settings:get_np_group("vmg_" .. flag) -- get this flag in minetest.conf
 		if on_config then -- This flag exists in minetest.conf, so return its value
-			vmg.settings:set(flag, on_config)
-			return vmg.string_to_noise(on_config), false
+			vmg.settings:set_np_group(flag, on_config)
+			return on_config, false
 		else -- Flag don't exist anywhere, so the default value will be written in settings and returned
-			local str_default = vmg.noise_to_string(default)
 			if write_to_config then
-				minetest.setting_set("vmg_" .. flag, str_default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
+				minetest.settings:set_np_group("vmg_" .. flag, default) -- write to minetest.conf if write_to_config is enabled (usually disabled)
 			end
-			vmg.settings:set(flag, str_default) -- write to vmg.conf
+			vmg.settings:set_np_group(flag, default) -- write to vmg.conf
 			return default, false -- return default value
 		end
 	end
 end
 
+local definefunc = {
+	string = define_str,
+	number = define_num,
+	boolean = define_bool,
+	table = define_noise,
+}
+
 function vmg.define(flag, default, write_to_config)
 	local typeval = type(default) -- Select function from the type of the default value
-	if typeval == "string" then
-		return define_str(flag, default, write_to_config)
-	elseif typeval == "number" then
-		return define_num(flag, default, write_to_config)
-	elseif typeval == "boolean" then
-		return define_bool(flag, default, write_to_config)
-	elseif typeval == "table" then
-		return define_noise(flag, default, write_to_config)
+	local f = definefunc[typeval] -- Choose the appropriate function
+	if f then
+		return f(flag, default, write_to_config)
 	end
-end
-
-function vmg.noise_to_string(n)
-	return n.offset ..
-		", " .. n.scale ..
-		", " .. minetest.pos_to_string(n.spread) ..
-		", " .. n.seed ..
-		", " .. n.octaves ..
-		", " .. n.persist ..
-		", " .. n.lacunarity
-end
-
-function vmg.string_to_noise(str)
-	local t = {}
-	for line in str:gmatch("[%d%.%-e]+") do
-		table.insert(t, tonumber(line))
-	end
-	return {
-		offset = t[1],
-		scale = t[2],
-		spread = {x=t[3], y=t[4], z=t[5]},
-		seed = t[6],
-		octaves = t[7],
-		persist = t[8],
-		lacunarity = t[9],
-	}
 end
 
 if vmg.loglevel >= 2 then
